bb492e8c90713a1532da44f9ac3c06410b9c005c
曹宇廷16924 <vvlove-16924@email.esunbank.com.tw>
2017-09-12 11:02:20 +0800

    Initial Commit

---
+++ FourSeason/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FourSeason
+//
+//  Created by 曹宇廷16924 on 2017/9/12.
+//  Copyright © 2017年 VTStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
